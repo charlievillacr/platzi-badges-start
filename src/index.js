44a76bbf946a31932ecd1f93169033039bae5701
charlievillacr <charlievilla@gmail.com>
2020-01-22 17:56:11 -0600

    Uso de React.createElement y JSX

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
-// Crear elemento
-const element = document.createElement('h1');
+// const elemento = React.createElement(
+//     'a',
+//     { href: 'https://platzi.com' },
+//     'Ir a Platzi.com'
+//     );
 
-// Asignarle un texto
-element.innerText = 'Helo, Platzi Badges';
+// const nombre = 'Charlie';
+// const suma = () => 3 + 3;
+// const elemento = React.createElement(
+//     'h1',
+//     {},
+//     `Hola, soy ${nombre}`
+// );
+// const jsx = <h1>Hola soy, {nombre}</h1>
+// const jsx = <h1>Hola soy, {2 + 2}</h1>
+// const jsx = <h1>Hola soy, {suma()}</h1>
+// const jsx = <h1>Hola soy, {null}</h1>
 
-// Donde queremos ponerlo
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// const jsx = <div>
+//                 <h1>Hola, soy Charlie</h1>
+//                 <p>Quiero ser ingeniero front end</p>
+//             </div>
+
+ const elemento = React.createElement(
+     'div',
+     {},
+     React.createElement('h1', {}, 'Hola, soy Carlos'),
+     React.createElement('p', {}, 'Quiero ser ingeniero frontend'),
+ )           
 const container = document.getElementById('app');
 
-container.appendChild(element);
+// ReactDOM.render(__qué__, __dónde__);
+ReactDOM.render(elemento, container);
