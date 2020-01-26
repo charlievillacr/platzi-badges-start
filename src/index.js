2f2ff6f2a1d0c6127aa3418e3ddb55bd63ccdaa2
charlievillacr <charlievilla@gmail.com>
2020-01-26 11:42:40 -0600

    Elemento en react

--- src/index.js
+++ src/index.js
@@ -1,34 +1,12 @@
-// const elemento = React.createElement(
-//     'a',
-//     { href: 'https://platzi.com' },
-//     'Ir a Platzi.com'
-//     );
-
-// const nombre = 'Charlie';
-// const suma = () => 3 + 3;
-// const elemento = React.createElement(
-//     'h1',
-//     {},
-//     `Hola, soy ${nombre}`
-// );
-// const jsx = <h1>Hola soy, {nombre}</h1>
-// const jsx = <h1>Hola soy, {2 + 2}</h1>
-// const jsx = <h1>Hola soy, {suma()}</h1>
-// const jsx = <h1>Hola soy, {null}</h1>
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const jsx = <div>
-//                 <h1>Hola, soy Charlie</h1>
-//                 <p>Quiero ser ingeniero front end</p>
-//             </div>
-
- const elemento = React.createElement(
-     'div',
-     {},
-     React.createElement('h1', {}, 'Hola, soy Carlos'),
-     React.createElement('p', {}, 'Quiero ser ingeniero frontend'),
+ const elemento = (
+    <div>
+        <h1>Hola, soy Carlos</h1>
+        <p>Aprendo ReactJS</p>
+    </div>
  )           
 const container = document.getElementById('app');
 
